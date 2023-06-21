c9abd1d8783085fff37db0327682bc00792ab301
ayoub Arif <Ichbinarif01@gmail.com>
2023-06-21 11:11:12 -0700

    #Project: 0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+
+#ifndef main_h
+#define main_h
+
+/*THIS IS THE HEADER FILE*/
+
+
+/*0_putchar.c*/
+int _putchar(char);
+/*1-alphabet.c*/
+void print_alphabet(void);
+
+/*2-print_alphabet_x10.c*/
+void print_alphabet_x10(void);
+
+/*3-islower.c*/
+int _islower(int c);
+
+/*4-alpha.c*/
+int _isalpha(int c);
+
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
+
